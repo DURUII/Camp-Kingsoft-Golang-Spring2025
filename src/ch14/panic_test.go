d986e7b39b0203dc8e6306a9daaf1d082fd1a21c
DURUII <wust.durui@qq.com>
2025-03-21 21:22:17 +0800

    feat: 错误处理demo

---
+++ src/ch14/panic_test.go
@@ -0,0 +1,39 @@
+package ch14
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestOSExit(t *testing.T) {
+	defer func() {
+		fmt.Println("Finally")
+	}()
+	fmt.Println("Started")
+	// defer 函数是不会执行的
+	os.Exit(-1)
+}
+
+func TestPanic(t *testing.T) {
+	defer func() {
+		fmt.Println("Finally")
+	}()
+	fmt.Println("Started")
+	// defer 函数会执行
+	panic(errors.New("fatal error"))
+}
+
+func TestRecover(t *testing.T) {
+	defer func() {
+		// 错误恢复机制 or “Let it Crash!”：避免僵尸进程
+		if err := recover(); err != nil {
+			fmt.Println("recovered from", err)
+		}
+	}()
+
+	fmt.Println("Started")
+	// defer 函数会执行
+	panic(errors.New("fatal error"))
+}
