8a799982e1a9a3bc1c655ee652a01c875455e893
DURUII <wust.durui@qq.com>
2025-03-21 21:22:17 +0800

    docs: 错误处理demo

---
+++ src/ch14/error_test.go
@@ -0,0 +1,36 @@
+package ch14
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+// 一般预置错误，为了后续区分错误类型
+var TooSmallError = errors.New("n must be no less than than 2")
+
+// 我们改造 GetFibonacciList 函数，支持错误校验
+func GetFibonacciList(n int) ([]int, error) {
+	if n < 2 {
+		return nil, TooSmallError
+	}
+
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList[:], nil
+}
+
+func TestError(t *testing.T) {
+	// Go 没有异常机制，没有 try ... catch ... finally
+	if val, err := GetFibonacciList(1); err != nil {
+		// 结构体的所有成员变量都是可比较的，那么结构体就可比较
+		if err == TooSmallError {
+			fmt.Println("It is less than 2.")
+		}
+		t.Log(err)
+	} else {
+		t.Log(val)
+	}
+}
