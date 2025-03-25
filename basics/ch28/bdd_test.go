f7d5c5d816eebc8e937d58250faaf568f8cb4ad5
DURUII <wust.durui@qq.com>
2025-03-25 08:36:06 +0800

    feat: BDD测试demo

---
+++ basics/ch28/bdd_test.go
@@ -0,0 +1,25 @@
+package ch28
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+// go install github.com/smartystreets/goconvey@latest
+// $GOPATH/bin/goconvey
+func TestSpec(t *testing.T) {
+
+	// Only pass t into top-level Convey calls
+	Convey("Given 2 even numbers", t, func() {
+		a := 2
+		b := 4
+
+		Convey("When add the two numbers", func() {
+			c := a + b
+
+			Convey("Then the result is still even", func() {
+				So(c%2, ShouldEqual, 0)
+			})
+		})
+	})
+}
