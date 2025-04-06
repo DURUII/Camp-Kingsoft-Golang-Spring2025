700540078b771fcfa2be54d5e2f5c5c50b35dca0
DURUII <wust.durui@qq.com>
2025-04-07 00:53:04 +0800

    feat: 线性数据结构

--- leetcode/线性数据结构/数组/26删除有序数组中的重复项.go
+++ leetcode/线性数据结构/数组/26删除有序数组中的重复项.go
@@ -2,8 +2,7 @@ package main
 
 // 过滤器模版题
 func removeDuplicates(nums []int) int {
-	// 代表长度，也代表写指针索引
-	w := 0
+	w := 0                           // 代表长度，也代表写指针索引
 	for r := 0; r < len(nums); r++ { // 读指针
 		if r == 0 || nums[r] != nums[r-1] { // 注意越界问题
 			nums[w] = nums[r]
